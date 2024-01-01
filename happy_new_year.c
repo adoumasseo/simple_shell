74d8a5e36f2318eda31f3f02cef136404e81c692
adoumasseo <adoumasseo@gmail.com>
2024-01-01 01:15:14 +0100

    Happy new year enginner

--- happy_new_year.c
+++ happy_new_year.c
@@ -2,5 +2,6 @@
 int main(void)
 {
     printf("Hello world here we are, May the coming year bring you success, happiness, and prosperity. Happy New Year!");
+    printf("i just have first fear of 2024 with by tokens :), he we go again")
     return (0);
 }
